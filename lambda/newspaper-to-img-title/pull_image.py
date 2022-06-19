2a1fa925af727009364421332d13b666c610aa88
AnastaciusWright <alexandronadal@gmail.com>
2022-06-19 12:26:47 +0200

    feat: images now will be obtained based on the event date from cloudwatch and not the current date. This will allow to bring images from dates different than the current date

--- lambda/newspaper-to-img-title/pull_image.py
+++ lambda/newspaper-to-img-title/pull_image.py
@@ -9,23 +9,31 @@ import boto3
     
 def pull_image(event, context):
     #s3 arn
+    # get date from event
+    date = event['Records'][0]['eventTime'].split('T')[0]
+    print("Date of event: " + date)
     s3_arn = 'arn:aws:s3:::clarin-image-bucket'
-    # get today's date
-    today = datetime.datetime.today()
+    # generate datetime object from date string
+    today = datetime.datetime.strptime(date, '%Y-%m-%d')
+    today_inverted = today.strftime('%d-%m-%Y')
     # url from clarin is:
-    clarin_url= ' https://www.clarin.com/tapa-diario-clarin-hoy.html?h='
-    #adding date at the end of url in format dd-mm-yyyy
-    clarin_url = clarin_url + today.strftime("%d-%m-%Y")
+    clarin_url= f'https://www.clarin.com/buscar-tapa-{today_inverted}.html'
+    print("Url to get image from: " + clarin_url)
     # using requests to get url, save html to a string
-    html = requests.get(clarin_url).text
-    
+    img_url = requests.get(clarin_url).json()['tapa_imagen']
+    print("Image url: " + img_url)
+    # I realized that it is not necessary to use beatiful soup because the image url also comes in a json response
+    # but I kept it for future reference
+
     # using beautifulsoup to parse html
-    soup = BeautifulSoup(html, 'html.parser')
+    # soup = BeautifulSoup(html, 'html.parser')
     # find the figure tag with class 'img-container'
-    img_container = soup.find('img', class_='img-responsive')
-    print(img_container)
+    #img_container = soup.find('img', class_='img-responsive')
+    #print(img_container)
     # obtain url
-    img_url = img_container['src']
+    #img_url = img_container['src']
+
+
     # get image from url and save it to a s3 bucket called image_clarin_bucket with name today's date
     img_data = requests.get(img_url).content
     # create a file object
@@ -34,15 +42,7 @@ def pull_image(event, context):
     img_binary = img_file.getvalue()
     # save img_binary to s3
     s3 = boto3.client('s3')
-    s3.put_object(Bucket='clarin-image-bucket', Key=f"front_pages/{today.strftime('%d-%m-%Y')}.webp", Body=img_binary)
-    # return the url of the image
-    body = {
-        "s3_url": s3_arn + '/' + today.strftime("%d-%m-%Y")
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
+    s3.put_object(Bucket='clarin-image-bucket', Key=f"front_pages/{today_inverted}.webp", Body=img_binary)
+    
+    
+    return 0
